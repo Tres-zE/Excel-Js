2acf5932ee3b3f4dff390ae2ed97f19f3ed9c604
rulo-dev <rulosan77@gmail.com>
2025-07-30 18:55:13 -0600

    añadiendo funciones para crear tablas

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-const $ = document.querySelector(el); //para seleccionar un elemento
-const $$ = document.querySelectorAll(el); //para seleccionar varios elementos
+const $ = (el) => document.querySelector(el); //para seleccionar un elemento
+const $$ = (el) => document.querySelectorAll(el); //para seleccionar varios elementos
+
+const ROWS = 10; //número de filas
+const COLUMNS = 3; //número de columnas
+
+const range = (length) => Array.from({ length }, (_, i) => i); //crea un array de longitud 'length' con índices del 0 al length-1
+
+const renderSpreadSheet = () => {
+  const $table = $('table');
+  const $head = $('thead');
+  const $body = $('tbody');
+
+  const headerHTML = `<tr>
+        <th></th> 
+        ${range(COLUMNS)
+          .map((i) => `<th>${i}</th>`)
+          .join('')}
+    </tr>`;
+
+  $head.innerHTML = headerHTML;
+
+  const bodyHTML = `<tr>
+    </tr>`;
+
+  $body.innerHTML = bodyHTML;
+};
+
+renderSpreadSheet(); // Inicializa la hoja de cálculo
